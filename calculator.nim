6b05e1f9b8795a8994c26d5a7e246d0c5d723270
Antricks <33262801+Antricks@users.noreply.github.com>
2021-12-12 15:55:42 +0100

    reduce parseutils imports

--- calculator.nim
+++ calculator.nim
@@ -22,7 +22,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 ]#
 
-import math, strutils, parseutils, tables
+import math, strutils, tables
+from parseutils import parseFloat
 
 const
     OPERATORS = {
